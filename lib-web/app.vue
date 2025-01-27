87a2b25d1655593f4bf7a971126b9be087667e42
TrafficLight6 <3214653283@qq.com>
2025-01-27 12:40:32 +0800

    add guest check

--- lib-web/app.vue
+++ lib-web/app.vue
@@ -6,14 +6,15 @@
 
 <script setup lang="ts">
 const config = useRuntimeConfig()
+
 const guest_allow: string = await $fetch(config.api_proxy + '/config/get?key=guest-allow')
 const guest_allow_json = JSON.parse(guest_allow)
 const router = useRouter()
 const cookie = useCookie('token')
+const user_check:string = await $fetch(config.api_proxy + '/user/check?token=' + cookie.value)
+const user_check_json = JSON.parse(user_check)
 
-
-
-if (guest_allow_json.resultMessage!='true'){
+if (guest_allow_json.resultMessage!='true' && user_check_json.success){
   router.push('/login')
 }
 </script>
