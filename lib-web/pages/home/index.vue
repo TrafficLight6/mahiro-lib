0fc3e190a516c08388203a3dd28a037bb072ed48
TrafficLight6 <3214653283@qq.com>
2025-02-03 15:28:23 +0800

    add bar in home.vue

--- lib-web/pages/home/index.vue
+++ lib-web/pages/home/index.vue
@@ -1,3 +1,36 @@
 <template>
-    
+    <div class="common-layout">
+        <el-container>
+            <el-header>
+                <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false">
+                    <el-menu-item index="0">
+                        <el-link href="/home">MahiroLib</el-link>
+                    </el-menu-item>
+                    <el-menu-item>All Book</el-menu-item>
+                    <el-menu-item>
+                        <el-input :prefix-icon="Search" clearable v-model="searchKeyWord"/>
+                        &ensp;&ensp;
+                        <el-link type="primary" :href="'/search?key_word='+searchKeyWord" @click="searchCheck">Search</el-link>
+                    </el-menu-item>
+                </el-menu>
+            </el-header>
+
+            <el-main></el-main>
+
+            <el-footer>Footer</el-footer>
+        </el-container>
+    </div>
 </template>
+
+<script setup>
+import { Search } from '@element-plus/icons-vue'
+
+const activeIndex = ref('1')
+let searchKeyWord = ref('')
+</script>
+
+<style>
+el-header {
+    background-color: rgb(102.2, 177.4, 255);
+}
+</style>
